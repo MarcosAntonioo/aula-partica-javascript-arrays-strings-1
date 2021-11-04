01ca79ab9e834264c8ab7750b4074b3507da35cf
Marcos2k02 <marquinhosmaciel44@gmail.com>
2021-11-04 14:32:47 -0300

    Arrays e strings

---
+++ script.js
@@ -0,0 +1,18 @@
+//atividade 1
+var dominio1 = "mundojs";
+var dominio2 = "google";
+
+console.log("Olá " + dominio1 + "! O site www." + dominio2 + ".com te ajuda a achar materiais de estudo.");
+
+//atividade 2
+var valorA = "casa";
+var valorB = "asa";
+
+console.log(valorA.toLocaleUpperCase());
+console.log(valorB.toLocaleUpperCase());
+console.log(valorA.localeCompare(valorB));
+
+//atividade 3
+var valorA = "substring(): Aprenda a utilizar.";
+
+console.log(valorA.substring(12));
